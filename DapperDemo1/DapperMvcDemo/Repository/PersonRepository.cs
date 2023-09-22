8e44a7b13d194c27c36f149a0c1cb01e61978aba
shubham verma <101853403+Shubham25122001@users.noreply.github.com>
2023-09-22 15:38:19 +0530

    Initial commit

---
+++ DapperDemo1/DapperMvcDemo/Repository/PersonRepository.cs
@@ -0,0 +1,65 @@
+﻿using DapperMvcDemo.Data.DataAccess;
+using DapperMvcDemo.Data.Models.Domain;
+
+namespace DapperMvcDemo.Data.Repository
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly ISqlDataAccess _db;
+
+        public PersonRepository(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+       public async Task<bool> AddAsync(Person person){
+            try
+            {
+                await _db.SaveData("SHUBHAM_sp_create_person", new { person.Name, person.Email, person.Address });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Person person)
+        {
+            try
+            {
+                await _db.SaveData("SHUBHAM_sp_update_person", person);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                await _db.SaveData("SHUBHAM_sp_delete_person", new { Id = id });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+   
+        public async Task<Person?> GetByIdAsync(int id)
+        {
+            IEnumerable<Person> result = await _db.GetData<Person, dynamic>("SHUBHAM_sp_get_person", new { Id = id });
+            return result.FirstOrDefault();
+        }
+    
+        public async Task<IEnumerable<Person>> GetAllAsync()
+        {
+            string query = "SHUBHAM_sp_getAll_person";
+            return await _db.GetData<Person, dynamic>(query, new { });
+        }
+    }
+}
---
+++ DepperForSelectQuery/DepperForSelectQuery/Program.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=practicedbdev.database.windows.net;Initial Catalog=PracticeDatabase;User ID=practicedb;Password=Admin@123";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string sqlQuery = "SELECT * FROM SHUBHAM_STUDENT_CRUD";
+            var student = connection.Query<Student>(sqlQuery);
+
+            Console.WriteLine("Id Name     FatherName   MotherName   Gender    DOB      AadharNo");
+
+            foreach (var item in student)
+            {
+                Console.WriteLine(item.Id + "" + item.Name + "" + item.FatherName + "" + item.MotherName + "" + item.Gender + "" + item.DOB + "" + item.AadharNo);
+            }
+        }
+    }
+}
+
+class Student
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string FatherName { get; set; }
+    public string MotherName { get; set; }
+    public String Gender { get; set; }
+    [DataType(DataType.DateTime)]
+    public DateTime DOB { get; set; }
+    public long AadharNo { get; set; }
+
+
+   
+}
+
+
+
+//"ConnectionStrings": {
+//    //"DBConn": "Data Source=bicspcore.database.windows.net;Initial Catalog=DemoDB;User ID=BITUser;Password=p@ssbi#word753"
+//    "DBConn": "Data Source=practicedbdev.database.windows.net;Initial Catalog=PracticeDatabase;User ID=practicedb;Password=Admin@123"
+//  //"DBConn": "Data Source=LOCALHOST\\SQLEXPRESS;Initial Catalog=Student;Integrated Security=True"
+//}
